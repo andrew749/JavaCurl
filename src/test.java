9090513f7d6a0ad82bdec4889c989e21119f1833
Andrew Codispoti <andrewcod749@gmail.com>
2015-02-01 00:21:51 -0500

    Inital commit.

---
+++ src/test.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class test {
+
+	public static void main(String[] args) {
+		Runnable d = new DownloadData(args[0]);
+		Thread t = new Thread(d);
+		t.start();
+	}
+
+	public static void doneLoadingDisplay(String text) {
+		System.out.print(text);
+		try {
+			FileOutputStream os = new FileOutputStream("test.txt");
+			os.write(text.getBytes());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class DownloadData implements Runnable {
+	public DownloadData(String url) {
+		try {
+			this.url = new URL(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	URL url;
+	InputStream is = null;
+	BufferedReader br;
+
+	public void run() {
+
+		try {
+			is = url.openStream();
+			br = new BufferedReader(new InputStreamReader(is));
+			test.doneLoadingDisplay(br.readLine());
+			is.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
